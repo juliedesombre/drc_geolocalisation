0457aef327505e569db79c3aebd65a3893101299
Grégoire Lurton <gregoire.lurton@gmail.com>
2020-01-27 14:19:41 +0100

    adding low dash cleaning

--- src/drc_fosa_geolocalisation/data_clean.py
+++ src/drc_fosa_geolocalisation/data_clean.py
@@ -3,6 +3,7 @@ import numpy as np
 import geopandas as gpd
 
 replace_dico = {"common":{
+                    "_":" ",
                     "-":" ",
                     "'":" ",
                     "ã¯":"i",
